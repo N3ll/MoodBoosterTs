b32636c7594a6437441c90a70e73b00d2fe5b8f3
Neli Ch <neli.chakarova@yahoo.com>
2015-11-08 20:19:56 +0200

    Images added

--- src/views/quiz/questions.ts
+++ src/views/quiz/questions.ts
@@ -2,12 +2,14 @@ import {QuestionsViewModel} from "./questions-view-model";
 import {EventData} from "data/observable";
 import {Button} from "ui/button";
 import {Page} from "ui/page";
-import { GestureEventData } from "ui/gestures"
+import {GestureEventData } from "ui/gestures"
+import {Image} from "ui/image";
 
 var viewModel: QuestionsViewModel;
+var page;
 
 export function navigatedTo(args: EventData) {
-	var page = <Page>args.object;
+	page = <Page>args.object;
 	viewModel = new QuestionsViewModel();
 	page.bindingContext = viewModel;
 }
@@ -15,3 +17,11 @@ export function navigatedTo(args: EventData) {
 export function saveAnswerTap(args: GestureEventData){
 	 viewModel.saveAnswer(args.view.parent.bindingContext,args.view.bindingContext);
 	}
+	
+export function animate(args:GestureEventData){
+	var imgView =<Image> page.getViewById("coffee");
+	imgView.animate({
+    scale: { x: 0, y: 2},
+    duration: 3000
+});
+}
