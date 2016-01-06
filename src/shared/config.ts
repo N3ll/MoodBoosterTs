5a68e70224b4ef26e7fcaf2336c10cc86c6fead0
Neli <neli.chakarova@yahoo.com>
2016-01-06 21:41:42 +0200

    new db

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,4 +1,4 @@
 export var Everlive = require ("./EverliveSDK.JS/everlive.all.min");
 export var Query = Everlive.Query;
-export var el = new Everlive({apiKey:"Q1Pf8VqN0bbqZGgB",scheme:"https"});
+export var el = new Everlive({apiKey:"53hx7l3k15w72jy5",scheme:"http"});
 
